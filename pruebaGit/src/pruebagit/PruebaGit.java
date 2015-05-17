41981017714910f923d6257b86ccdb33b9cb3799
iban2 <ibanhidalgo33@hotmail.com>
2015-05-17 21:12:01 +0200

    cambios
    eso cambios

--- pruebaGit/src/pruebagit/PruebaGit.java
+++ pruebaGit/src/pruebagit/PruebaGit.java
@@ -16,6 +16,10 @@ public class PruebaGit {
      */
     public static void main(String[] args) {
         System.out.println("cambio realizado de prueba");
+          System.out.println("cambio realizado de prueba");
+            System.out.println("cambio realizado de prueba");
+              System.out.println("cambio realizado de prueba");
+                System.out.println("cambio realizado de prueba");
     }
     
 }
